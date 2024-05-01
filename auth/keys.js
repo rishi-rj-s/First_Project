62a9fb5f1db8485b610848deda6f0ade88af406f
rishi-rj-s <rishisanu007@gmail.com>
2024-05-01 20:33:47 +0530

    Final touchings for hosting

---
+++ auth/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+     google: {
+          clientID: '1053402896061-77lvkv2vekft4bg7muobvhftup6vfojp.apps.googleusercontent.com',
+          clientSecret:'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'
+     }
+}
